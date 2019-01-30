a373e35131a2dc0276f8b30cce47838bd4364147
nikk charalampidis <nikk.ch@protonmail.com>
2019-01-30 17:57:04 +0200

    python3 update

--- ransSamp.py
+++ ransSamp.py
@@ -1,87 +1,54 @@
-#!/usr/local/bin/python2.7
-from __future__ import unicode_literals
-import request
-import requests
-import subprocess
+"""
+GOAL:
+this script will optain a random youtube video id.
+then download 1 min of that videos audio and then notify sclang via OSC message.
+
+steps:
+1. send osc message
+2. optain random youtube video id
+3. download 1 min audio
+4. send osc message
+5. wrap to whole thing in a 1 min loop
+
+"""
 import json
-import OSC
-import csv
-import datetime
-import time, random
-import threading
-import shutil
+import urllib.request
+import string
+import random
+import os
+import argparse
+import time
 
-counter = 0
-t2n = 60;
-vidIDlist = [];
-client = OSC.OSCClient()
-client.connect( ( '127.0.0.1', 57120 ) )
-msg = OSC.OSCMessage()
-msg.setAddress("/newsample")
-msg.append(45)
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
-url = "https://randomyoutube.net/api/getvid?api_token=JHhvy16oOiSmQwB27eTmNaASvsduVUx2bCmCvBYpkbtGg5Tp9AyLGjKbK71t"
+# random video id
+count = 1
+API_KEY = 'AIzaSyDjFs6JULeueFkFPUZli9K5kDjsg2VN4Rk'
+while(True):
+  randomYt = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+  urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(API_KEY,count,randomYt)
+  webURL = urllib.request.urlopen(urlData)
+  data = webURL.read()
+  encoding = webURL.info().get_content_charset('utf-8')
+  results = json.loads(data.decode(encoding))
 
-def handler(addr, tags, data, client_address):
-    txt = "OSCMessage '%s' from %s: " % (addr, client_address)
-    txt += str(data)
-    if data[0] == 'saveSamplePlz':
-        lastRow = open('idList.csv', 'r').readlines()[-1]
-        shutil.copyfile('abc.wav','selected/'+lastRow+'.wav')
-    # elif data[0] == 't2n':
-    #     t2n = data[1]
-    print(txt)
-
-def audioReq():
-    vidIDlist = []
-    r = requests.get(url)
-    vidResp = json.loads(r.content)
-    print (vidResp['vid'])
-    vidIDlist.append(datetime.datetime.now())
-    vidIDlist.append(vidResp['vid'])
-    with open('idList.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(vidIDlist)
-        ytUrl = 'https://www.youtube.com/watch?v='+ vidResp['vid']
-        print(ytUrl)
-        audioStr0 = 'youtube-dl --get-duration '+ ytUrl
-        process = subprocess.Popen(audioStr0.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        durArr = output.split(':')
-        print(len(durArr))
-        audioStr = 'youtube-dl -f bestaudio[ext=m4a] -g '+ ytUrl
-        process = subprocess.Popen(audioStr.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        audioStream = output[:-1]
-        print(audioStream)
-    if len(durArr) == 1:
-        downBash = 'ffmpeg -i \''+ audioStream +'\' -ac 1 -f wav ~/randSynth/abc.wav -y'
-        process2 = subprocess.call(downBash, stdout=subprocess.PIPE, shell=True)
-        # output2, error2 = process.communicate()
-        # print(output2)
-        print(downBash)
-    else:
-        downBash = 'ffmpeg -i \''+ audioStream +'\' -ss 00:00:00 -t 00:01:00 -ac 1 -f wav ~/randSynth/abc.wav -y'
-        process2 = subprocess.call(downBash, stdout=subprocess.PIPE, shell=True)
-        # output2, error2 = process.communicate()
-        # print(output2)
-        print(downBash)
-
-def downLoop():
-    while True:
-        global counter
-        global t2n
-        counter += 1
-        print("counter", counter)
-        audioReq()
-        client.send(msg)
-        time.sleep(t2n)
-def saveSample():
-        s = OSC.OSCServer(('127.0.0.1', 50010))  # listen on localhost, port 57120
-        s.addMsgHandler('/newsample', handler)     # call handler() for OSC messages received with the /startup address
-        s.serve_forever()
-thread1 = threading.Thread(target=downLoop)
-thread1.start()
-
-thread2 = threading.Thread(target=saveSample)
-thread2.start()
+  for data in results['items']:
+    videoId = (data['id']['videoId'])
+  print(videoId)
+  # get youtube video duration
+  ytUrl = 'https://www.youtube.com/watch?v=' + videoId
+  videoDur = os.popen('youtube-dl --get-duration ' + ytUrl).read()
+  # audio stream of video
+  # [:-1] removes the new line the os.popen returns
+  audioStr = os.popen('youtube-dl -f \'bestaudio[ext=m4a]\' -g ' + ytUrl).read()[:-1]
+  # if video length is smaller than 1 minute then download the whole length else
+  # download the first minute
+  if(len(videoDur)==3):
+    os.system('ffmpeg -i \''+ audioStr +'\' -ac 1 -f wav ./noise.wav -y')
+  else:
+    os.system('ffmpeg -i \''+ audioStr +'\' -ss 00:00:00 -t 00:01:00 -ac 1 -f wav ./noise.wav -y')
+  # OSC
+  client = udp_client.SimpleUDPClient("127.0.0.1", 52170)
+  client.send_message("/reloadSample", videoId)
+  time.sleep(15)
